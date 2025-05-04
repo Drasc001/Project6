dfe8de1cb6156d73741dca105021cd330d4a7517
Drasc001 <dtr2170@email.vccs.edu>
2025-05-04 17:18:38 -0400

    Commit

---
+++ Project3_Temp/Project3_Temp/Project3_Temp.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+// Function to convert Fahrenheit to Celsius
+double celsius(int fahrenheit) {
+    return (5.0 / 9.0) * (fahrenheit - 32);
+}
+
+// Function to display the Celsius table
+void showCelsiusTable() {
+    cout << "Fahrenheit\tCelsius\n";
+    cout << "------------------------\n";
+
+    for (int f = 0; f <= 20; f++) {
+        cout << setw(9) << f << "\t" << fixed << setprecision(2) << celsius(f) << endl;
+    }
+}
+
+int main() {
+    showCelsiusTable();
+    return 0;
+}
